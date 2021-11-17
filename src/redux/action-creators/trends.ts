7fbd5a0a1b90b3bca6ceb72102004ffbfbcfbac4
Nazarii Kovalenko <nazarii.dev@gmail.com>
2021-11-17 23:02:57 +0200

    changed api key

--- src/redux/action-creators/trends.ts
+++ src/redux/action-creators/trends.ts
@@ -4,17 +4,17 @@ import { TrendsActions, ITrendsAction } from '../types/trends';
 
 export const fetchTrends = () => {
   return async (dispatch: Dispatch<ITrendsAction>) => {
-    try { 
+    try {
       dispatch({ type: TrendsActions.TRENDS_FETCH });
       const response = await axios.get('https://tiktok33.p.rapidapi.com/trending/feed', {
         headers: {
           'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
-          'x-rapidapi-key': '16da4ea503msh032c64c7a11bfe4p14071djsn35feb5305f81'
-        }
-      })
+          'x-rapidapi-key': 'c1257dc04cmshd888bbb072eb770p1f2b8ajsnbf16d4cd1d66',
+        },
+      });
       dispatch({ type: TrendsActions.TRENDS_FETCH_SUCCESS, payload: response.data });
-    } catch(error) {
+    } catch (error) {
       dispatch({ type: TrendsActions.TRENDS_FETCH_ERROR });
     }
-  }
-}
+  };
+};
--- src/redux/action-creators/user.ts
+++ src/redux/action-creators/user.ts
@@ -23,7 +23,7 @@ export const fetchUserFeed = (name: string) => async (dispatch: Dispatch<UserAct
     const response = await axios.get(`https://tiktok33.p.rapidapi.com/user/feed/${name}`, {
       headers: {
         'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
-        'x-rapidapi-key': '16da4ea503msh032c64c7a11bfe4p14071djsn35feb5305f81',
+        'x-rapidapi-key': 'c1257dc04cmshd888bbb072eb770p1f2b8ajsnbf16d4cd1d66',
       },
     });
     dispatch({ type: EnumUserAction.USER_FETCH_FEED_SUCCESS, payload: response.data });
